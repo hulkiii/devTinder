a2a48d3e40d03cd2de966a6c6bf4eb6972a92c87
shubham <shubham@gmail.com>
2025-05-01 20:47:22 +0530

    exploring get,post,delete,patch

--- src/app.js
+++ src/app.js
@@ -1,13 +1,32 @@
 const express = require ("express")
 const app = express();
 
-app.use("/", (req, res) =>{
-    res.send("namste dashboard");
-});
+app.get("/user", (req,res) =>{
+    res.send("get the user data");
+})
+app.post("/user", (req,res) =>{
+    res.send("post the users data")
+})
+app.delete("/user", (req, res ) =>{
+    res.send("deleted the user")
+})
+app.put("/user", (req, res) =>{
+    res.send("put(update whole) the user data")
+})
+
+app.use("/test/test3", (req, res) =>{
+    res.send("welcome to test3 pagee");
+})
+app.use("/test/2", (req, res) =>{
+    res.send("welcome to test2 pagee");
+})
 
 app.use("/test", (req, res) =>{
     res.send("welcome to test pagee");
 })
+app.use("/", (req, res) =>{
+    res.send("namste dashboard");
+});
 
 app.listen(7777, ()=>{
     console.log("running on 7777 port");
