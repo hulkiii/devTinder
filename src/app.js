a11980c11cc3ea485f7e8b927e6a9fec3de66795
shubham <shubham@gmail.com>
2025-05-06 23:14:05 +0530

    delete an update

--- src/app.js
+++ src/app.js
@@ -2,19 +2,70 @@ const express = require ("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user")
-
+app.use(express.json());
 app.post("/signup", async (req , res) =>{
     //creating a new instance of user model
-    const user = new User({
-       firstName:"Sachin",
-       lastName:"Tendular",
-       emailId: "sachin@gmai.com",
-       password: "sachin@123"
+    const user = new User(req.body);
+    try{
+        await user.save();
+        res.send("user added sucessfully");
+    }catch (err){
+        res.status(400).send("error saving the user:" + err.message);
+    }
+    
+});
 
-    });
-    await user.save();
-    res.send("user added sucessfully!");
+app.get("/user", async (req, res) =>{
+    const userEmail = req.body.emailId;
+    try{
+        const users = await User.find({emailId: userEmail});
+        if(users.length === 0){
+            res.status(400).send("users not found");
+        }else{
+            res.send(users);
+        }
+       
+    }catch (err){
+        res.status(400).send("something went wrong");
+    }
 })
+
+//feed api
+app.get("/feed", async (req , res) =>{
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch (err){
+        res.status(400).send("something went wrong");
+    }
+})
+
+app.delete("/user", async (req ,res) =>{
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete({_id: userId});
+        res.send("user deleted sucessfully");
+
+    }
+    catch (err){
+        res.status(400).send("something went wrong");
+    }
+})
+
+//update data of the user
+app.patch("/user", async (req ,res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try{
+        const user= await User.findByIdAndUpdate({_id: userId}, data,{
+            returnDocument: "before",
+        });
+        console.log(user);
+        res.send("user updates sucesssfully");
+    }catch(err){
+        res.status(400).send("something went wrong");
+    }
+});
 
 connectDB()
 .then(() => {
