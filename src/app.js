9ae9780ffdffbd29ac1ca1c2efb75573a81952cd
shubham <shubham@gmail.com>
2025-05-07 23:35:25 +0530

    validator , bcrypt, user login

--- src/app.js
+++ src/app.js
@@ -1,81 +1,235 @@
-const express = require ("express");
-const connectDB = require("./config/database");
-const app = express();
-const User = require("./models/user")
-app.use(express.json());
-app.post("/signup", async (req , res) =>{
-    //creating a new instance of user model
-    const user = new User(req.body);
-    try{
-        await user.save();
-        res.send("user added sucessfully");
-    }catch (err){
-        res.status(400).send("error saving the user:" + err.message);
-    }
+// const express = require ("express");
+// const connectDB = require("./config/database");
+// const app = express();
+// const User = require("./models/user")
+// const  validateSignUpData = require("./utils/validation");
+// const bcrypt = require('bcrypt');
+// app.use(express.json());
+// app.post("/signup", async (req , res) =>{
+//     //creating a new instance of user model
+//     const user = new User(req.body);
+//     try{
+//         await user.save();
+//         res.send("user added sucessfully");
+//     }catch (err){
+//         res.status(400).send("error saving the user:" + err.message);
+//     }
     
+// });
+
+// app.get("/user", async (req, res) =>{
+//     const userEmail = req.body.emailId;
+//     try{
+//         const users = await User.find({emailId: userEmail});
+//         if(users.length === 0){
+//             res.status(400).send("users not found");
+//         }else{
+//             res.send(users);
+//         }
+       
+//     }catch (err){
+//         res.status(400).send("something went wrong");
+//     }
+// })
+
+// //feed api
+// app.get("/feed", async (req , res) =>{
+//     try{
+//         const users = await User.find({});
+//         res.send(users);
+//     }catch (err){
+//         res.status(400).send("something went wrong");
+//     }
+// })
+
+// app.delete("/user", async (req ,res) =>{
+//     const userId = req.body.userId;
+//     try{
+//         const user = await User.findByIdAndDelete({_id: userId});
+//         res.send("user deleted sucessfully");
+
+//     }
+//     catch (err){
+//         res.status(400).send("something went wrong");
+//     }
+// })
+
+// //update data of the user
+// app.patch("/user", async (req ,res) => {
+//     const userId = req.body.userId;
+//     const data = req.body;
+//     try{
+//         const ALLOWED_UPDATES = ["photoUrl","about", "gender", "age", "skills"];
+//         const isUpdateAllowed = Object.keys(data).every((k) => 
+//             ALLOWED_UPDATES.includes(k)
+//         );
+//         if(!isUpdateAllowed){
+//             throw new Error("update not allowed")
+//         }
+//         if (data.skills && data.skills.length > 10) {
+//             throw new Error("Skills cannot be more than 10");
+//         }
+//         const user = await User.findByIdAndUpdate(userId, data,{
+//             returnDocument: "after",
+//             runValidators: true,
+//         });
+//         console.log(user);
+//         res.send("user updates sucesssfully");
+//     }catch(err){
+//         res.status(400).send("updte failed" + err.message);
+//     }
+// });
+
+// app.post("/login", async (req, res)=>{
+//     try{
+//         const {emailId, password} = req.body;
+//         const user = await User.findOne({emailId: emailId});
+//         if(!user){
+//             throw new Error ("Invalid credentials");
+//         }
+//         const isPasswordValid = await bcrypt.compare(password, user.password);
+//         if(isPasswordValid){
+//             res.send("login successfull !!..");
+//         }else{
+//             throw new Error ("Invalid credentials");
+//         }
+//     }catch (err){
+//         res.status(400).send("error :" + err.message);
+//     }
+// });
+
+// connectDB()
+// .then(() => {
+//     console.log("Databse connection established...");
+//     app.listen(7777, ()=>{
+//         console.log("running on 7777 port");
+//     })
+// })
+// .catch((err) =>{
+//     console.error("datbase cannot be connected !...");
+// });
+
+
+
+
+
+
+const express = require("express");
+const connectDB = require("./config/database");
+const bcrypt = require("bcrypt");
+const User = require("./models/user");
+
+const app = express();
+app.use(express.json());
+
+// Signup
+app.post("/signup", async (req, res) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.send("User added successfully");
+    } catch (err) {
+        res.status(400).send({ error: "Error saving the user: " + err.message });
+    }
 });
 
-app.get("/user", async (req, res) =>{
-    const userEmail = req.body.emailId;
-    try{
-        const users = await User.find({emailId: userEmail});
-        if(users.length === 0){
-            res.status(400).send("users not found");
-        }else{
-            res.send(users);
-        }
-       
-    }catch (err){
-        res.status(400).send("something went wrong");
-    }
-})
+// Login
+app.post("/login", async (req, res) => {
+    try {
+        const { emailId, password } = req.body;
 
-//feed api
-app.get("/feed", async (req , res) =>{
-    try{
+        if (!emailId || !password) {
+            return res.status(400).send({ error: "Email and password are required" });
+        }
+
+        const user = await User.findOne({ emailId });
+        if (!user) throw new Error("Invalid credentials");
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) throw new Error("Invalid credentials");
+
+        res.send("Login successful!");
+    } catch (err) {
+        res.status(400).send({ error: err.message });
+    }
+});
+
+// Get user by email
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+    try {
+        const users = await User.find({ emailId: userEmail });
+        if (users.length === 0) {
+            return res.status(404).send({ error: "User not found" });
+        }
+        res.send(users);
+    } catch (err) {
+        res.status(400).send({ error: "Something went wrong" });
+    }
+});
+
+// Feed (get all users)
+app.get("/feed", async (req, res) => {
+    try {
         const users = await User.find({});
         res.send(users);
-    }catch (err){
-        res.status(400).send("something went wrong");
+    } catch (err) {
+        res.status(400).send({ error: "Something went wrong" });
     }
-})
+});
 
-app.delete("/user", async (req ,res) =>{
+// Delete user
+app.delete("/user", async (req, res) => {
     const userId = req.body.userId;
-    try{
-        const user = await User.findByIdAndDelete({_id: userId});
-        res.send("user deleted sucessfully");
-
+    try {
+        const user = await User.findByIdAndDelete(userId);
+        if (!user) return res.status(404).send({ error: "User not found" });
+        res.send("User deleted successfully");
+    } catch (err) {
+        res.status(400).send({ error: "Something went wrong" });
     }
-    catch (err){
-        res.status(400).send("something went wrong");
-    }
-})
+});
 
-//update data of the user
-app.patch("/user", async (req ,res) => {
+// Update user
+app.patch("/user", async (req, res) => {
     const userId = req.body.userId;
     const data = req.body;
-    try{
-        const user= await User.findByIdAndUpdate({_id: userId}, data,{
-            returnDocument: "before",
+
+    try {
+        const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+        const isUpdateAllowed = Object.keys(data).every((k) =>
+            ALLOWED_UPDATES.includes(k)
+        );
+
+        if (!isUpdateAllowed) {
+            throw new Error("Update not allowed");
+        }
+
+        if (data.skills && data.skills.length > 10) {
+            throw new Error("Skills cannot be more than 10");
+        }
+
+        const user = await User.findByIdAndUpdate(userId, data, {
+            new: true,
+            runValidators: true,
         });
-        console.log(user);
-        res.send("user updates sucesssfully");
-    }catch(err){
-        res.status(400).send("something went wrong");
+
+        if (!user) return res.status(404).send({ error: "User not found" });
+
+        res.send("User updated successfully");
+    } catch (err) {
+        res.status(400).send({ error: "Update failed: " + err.message });
     }
 });
 
+// Connect DB and start server
 connectDB()
-.then(() => {
-    console.log("Databse connection established...");
-    app.listen(7777, ()=>{
-        console.log("running on 7777 port");
+    .then(() => {
+        console.log("Database connection established...");
+        app.listen(7777, () => {
+            console.log("Server running on port 7777");
+        });
     })
-})
-.catch((err) =>{
-    console.error("datbase cannot be connected !...");
-});
-
-
+    .catch((err) => {
+        console.error("Database connection failed!", err);
+    });
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,145 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+// const validator = require("validator")
+// const userSchema = new mongoose.Schema(
+//     {
+//         firstName: {
+//             type: String,
+//             required: true,
+//             minLength: 4,
+//             maxLength: 30,
+//         },
+//         lastName:{
+//             type: String,
+//         },
+//         emailId:{
+//             type: String,
+//             lowercase: true,
+//             required: true,
+//             unique: true,
+//             trim: true,
+//             validator(value){
+//                 if(!validator.isEmail(value)){
+//                     throw new Error ("invalid email address:" +value);
+//                 }
+//             }
+//         },
+//         password:{
+//             type: String,
+//             required: true,
+//             validate(value){
+//                 if(validator.isStrongPassword(value)){
+//                     throw new Error("enter a strong password:" +value);
+//                 }
+//             }
+//         },
+//         age: {
+//             type: Number,
+//             min: 18,
+//         },
+//         gender:{
+//             type: String,
+//             validate(value){
+//                 if(!["male","female","others"].includes(value)){
+//                     throw new Error("gender data is not valid");
+//                 }
+//             }
+//         },
+//         photoUrl:{
+//             type: String,
+//             default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.marvel.com%2Fmovies%2Favengers-endgame&psig=AOvVaw0r7g8USDIu-LeD1-MD0j4w&ust=1746640641339000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMjknMu1j40DFQAAAAAdAAAAABAE",
+//         },
+//         about:{
+//             type: String,
+//             default: "this is the default about of the user"
+//         },
+//         skills:{
+//             type:[String],
+//         },
+//     },
+//     {
+//         timestamps: true,
+//     }
+// );
+// module.exports = mongoose.model("User", userSchema);
 
-const userSchema = new mongoose.Schema({
+
+
+
+
+
+
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String,
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 30,
     },
-    lastName:{
-        type: String,
+    lastName: {
+      type: String,
     },
-    emailId:{
-        type: String,
+    emailId: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address: " + value);
+        }
+      },
     },
-    password:{
-        type: String,
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Password is not strong enough: " + value);
+        }
+      },
     },
     age: {
-        type: Number,
+      type: Number,
+      min: 18,
     },
-    gender:{
-        type: String,
-    }
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.marvel.com%2Fmovies%2Favengers-endgame",
+    },
+    about: {
+      type: String,
+      default: "this is the default about of the user",
+    },
+    skills: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const bcrypt = require("bcrypt");
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
 
 module.exports = mongoose.model("User", userSchema);
