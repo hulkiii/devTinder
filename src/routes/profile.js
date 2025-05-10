a399e89efc24fe7000bc41242767737c4f58c50b
shubham <shubham@gmail.com>
2025-05-10 23:03:48 +0530

    logout  api and profile/edit pi created (which fileds need to edit is written in validation .js)

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const profileRouter = express.Router();
-
+const {validateEditProfileData} = require("../utils/validation")
 const{userAuth}  = require("../middewares/auth");
+
 
 profileRouter.get("/profile", userAuth, async (req, res) =>{
     try{
@@ -12,5 +13,22 @@ profileRouter.get("/profile", userAuth, async (req, res) =>{
     }
     
 });
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) =>{
+    try{
+        if(!validateEditProfileData(req)){
+            throw new Error ("invalid edit request")
+
+        }
+        const loggedInUser = req.user;
+        console.log(loggedInUser);
+        Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+        console.log(loggedInUser);
+        res.send(`${loggedInUser}, user profile edit sucessfully`);
+    }catch (err){
+        res.status(400).send("error:" + err.message);
+    }
+})
+
 
 module.exports = profileRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -10,6 +10,23 @@ const validateSignUpData = (req) =>{
         throw new Error ("please a strong password");
     }
 };
+
+const validateEditProfileData = (req) =>{
+    const allowedEditFields = [
+        "firstName",
+        "lastName",
+        "emailId",
+        "photoUrl",
+        "gender",
+        "age",
+        "about",
+        "skills",
+    ];
+    const isEditAllowed = Object.keys(req.body).every((field) =>
+        allowedEditFields.includes(field)
+    );
+    return isEditAllowed;
+}
 module.exports = {
-    validateSignUpData,
+    validateSignUpData,validateEditProfileData,
 };
